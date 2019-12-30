98ca655b00770247a629f46b7380b1e6ddbccb24
Dimas <dimas.maulanads@gmail.com>
2019-12-30 18:24:47 +0700

    Fix fallback fetch error message

--- assets/script/main.js
+++ assets/script/main.js
@@ -13,7 +13,7 @@ const page = {
 const fetchApi = url => {
     return fetch(url).then(response => {
         if(response.status !== 200) {
-            return Promise.reject(response.statusText)
+            return Promise.reject(response.status)
         }
         return response.json();
     })
